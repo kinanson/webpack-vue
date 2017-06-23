795e24b4d078fa4d788f6c656e1af73e6611c1e4
DESKTOP-FPFND5E\anson <kinanson@yahoo.com.tw>
2017-06-24 07:48:44 +0800

    added algo.vue

---
+++ src/components/Algo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="button" @click="click" value="click"> {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Algo',
+  data () {
+    return {
+      message: 'message'
+    }
+  },
+  methods: {
+    click () {
+      console.log('test')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
